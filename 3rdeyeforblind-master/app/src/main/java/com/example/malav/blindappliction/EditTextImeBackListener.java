d193b80d9693ebb1456b99ad049385e72bdca6c6
panktisugandhi <panktisugandhi143@gmail.com>
2017-04-05 17:36:33 +0530

    1 year Project is completed finally successful run

---
+++ 3rdeyeforblind-master/app/src/main/java/com/example/malav/blindappliction/EditTextImeBackListener.java
@@ -0,0 +1,6 @@
+package com.example.malav.blindappliction;
+
+public interface EditTextImeBackListener
+	{
+	public abstract void onImeBack(EditTextBackEvent ctrl, String text);
+	}
---
+++ 3rdeyeforblind-master/app/src/main/java/com/example/malav/blindappliction/MmsReceiver.java
@@ -0,0 +1,13 @@
+package com.example.malav.blindappliction;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MmsReceiver extends BroadcastReceiver
+	{
+    @Override public void onReceive(Context context, Intent intent)
+    	{
+        // TODO: Implement MMS receiver
+    	}
+	}
